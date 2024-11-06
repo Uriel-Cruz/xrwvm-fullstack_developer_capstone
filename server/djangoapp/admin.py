b83da90a65869c76f790184576d0b94e422096d5
Uriel-Cruz <uriel_26@ciencias.unam.mx>
2024-11-06 10:43:39 -0500

    adding models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,17 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
-# Register your models here.
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country_of_origin')  
+    search_fields = ('name', 'country_of_origin')  
 
-# CarModelInline class
 
-# CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year')
+    list_filter = ('type', 'year')  
+    search_fields = ('name', 'car_make__name')  
 
-# CarMakeAdmin class with CarModelInline
 
-# Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
