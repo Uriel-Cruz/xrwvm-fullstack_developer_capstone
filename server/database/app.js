ac5f1ae50a9dca0aef8bb8386f7963ab716da7af
Uriel-Cruz <uriel_26@ciencias.unam.mx>
2024-11-06 10:03:15 -0500

    adding endpoints

--- server/database/app.js
+++ server/database/app.js
@@ -58,19 +58,46 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
-});
+    try {
+      const dealers = await Dealerships.find();
+      res.json(dealers);
+    } catch (error) {
+      res.status(500).json({ error: 'Error fetching dealerships' });
+    }
+  });
+  
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
-});
+    try {
+      const dealers = await Dealerships.find({ state: new RegExp(`^${req.params.state}$`, "i") });
+      if (dealers.length > 0) {
+        res.json(dealers);
+      } else {
+        res.status(404).json({ error: 'No dealerships found in this state' });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error fetching dealerships by state' });
+    }
+  });
+  
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
-});
-
+    try {
+      const dealer = await Dealerships.findOne({ id: parseInt(req.params.id) }); // Buscamos por el campo `id`
+      if (dealer) {
+        res.json(dealer);
+      } else {
+        res.status(404).json({ error: 'Dealership not found' });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error fetching dealership by ID' });
+    }
+  });
+  
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
