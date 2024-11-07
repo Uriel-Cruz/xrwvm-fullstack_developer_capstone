04f2ec5f3fc6f8d59aef4c83ffc3cdcd818dbd91
Uriel-Cruz <uriel_26@ciencias.unam.mx>
2024-11-07 15:41:57 -0500

    new changes

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Register from "./components/Register/Register";
 import Dealers from './components/Dealers/Dealers';
 import PostReview from "./components/Dealers/PostReview"
+import Dealer from "./components/Dealers/Dealer"
 
 function App() {
   return (
@@ -10,8 +11,7 @@ function App() {
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dealers" element={<Dealers/>} />
-      <Route path="/dealer/:id" element={<Dealers/>} />
-      <Route path="/postreview/:id" element={<PostReview/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
     </Routes>
   );
 }
